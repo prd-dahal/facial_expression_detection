8d75a017d6e79f05bca0e705e0121a9f2edc38c6
prd-dahal <rhyme@ip-172-31-247-13.ec2.internal>
2020-05-09 15:14:38 +0000

    initial_commit

---
+++ model.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.models import model_from_json
+import tensorflow as tf
+import numpy as np
+#
+# config = tf.compat.v1.ConfigProto()
+# config.gpu.options.per_process_gpu_memeory_fraction = 0.5
+# session = tf.compat.v1.Session(config=config)
+
+class FacialExpressionModel(object):
+    EMOTIONS_LIST = ['Angry',"Disgust",'Fear','Happy','Neutral','Sad','Surpirse']
+    def __init__(self, model_json_file, model_weights_file):
+        with open(model_json_file, 'r') as json_file:
+            loaded_model_json = json_file.read()
+            self.loaded_model = model_from_json(loaded_model_json)
+        self.loaded_model.load_weights(model_weights_file)
+        #self.loaded_model.make_predict_function()
+    def predict_emotion(self, img):
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTIONS_LIST(np.argmax(self.preds))
